a9cfce29374dc540352e47d1d0fa535be03579a5
Atze de Vries <AtzeDeVries@users.noreply.github.com>
2022-06-29 16:07:12 +0200

    test

--- tests/test.sh
+++ tests/test.sh
@@ -6,7 +6,7 @@ token=${1}
 test_tf_update_action() {
   echo Testing ${1}, expecting ${2}
   interest=$(../entrypoint.sh $token ${1} no_token dryrun | grep -A 2 helloworld_github )
-  if ! echo $interest | grep -q ${2} ; then
+  if ! echo $interest | grep -q "${2}" ; then
     echo Failed. Found:
     echo $interest
     exit 1
