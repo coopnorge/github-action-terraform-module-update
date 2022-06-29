61cf9b34e41742b15cc93cdd3999af8e02d97bc5
Atze de Vries <AtzeDeVries@users.noreply.github.com>
2022-06-29 15:31:41 +0200

    fix test

--- tests/test.sh
+++ tests/test.sh
@@ -21,11 +21,11 @@ test_tf_update_action modules/pinned "\\-  version = \"1.12.0\""
 
 
 
-echo Testing beta versions
-../entrypoint.sh $token modules/beta no_token dryrun | grep "\\-  version = \"1.12.0-beta1\"" || (echo Failed. Should return: \'-  version = \"1.12.0-beta1\"\' ; exit 1)
-
-echo Testing follow versions
-../entrypoint.sh $token modules/follow no_token dryrun | grep "\\-  version = \"~> 1.0\"" || (echo Failed. Should return: \'-  version = \"1.12.0-beta1\"\' ; exit 1)
-
-echo Testing pinned versions
-../entrypoint.sh $token modules/pinned no_token dryrun | grep "\\-  version = \"1.12.0\"" || (echo Failed. Should return: \'-  version = \"1.12.0-beta1\"\' ; exit 1)
+#echo Testing beta versions
+#../entrypoint.sh $token modules/beta no_token dryrun | grep "\\-  version = \"1.12.0-beta1\"" || (echo Failed. Should return: \'-  version = \"1.12.0-beta1\"\' ; exit 1)
+#
+#echo Testing follow versions
+#../entrypoint.sh $token modules/follow no_token dryrun | grep "\\-  version = \"~> 1.0\"" || (echo Failed. Should return: \'-  version = \"1.12.0-beta1\"\' ; exit 1)
+#
+#echo Testing pinned versions
+#../entrypoint.sh $token modules/pinned no_token dryrun | grep "\\-  version = \"1.12.0\"" || (echo Failed. Should return: \'-  version = \"1.12.0-beta1\"\' ; exit 1)
