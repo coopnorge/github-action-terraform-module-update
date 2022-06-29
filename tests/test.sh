d1df645efa2f4efac02f3d603150c2f7395caaa5
Atze de Vries <AtzeDeVries@users.noreply.github.com>
2022-06-29 16:08:17 +0200

    bla

--- tests/test.sh
+++ tests/test.sh
@@ -7,7 +7,8 @@ test_tf_update_action() {
   echo Testing ${1}, expecting ${2}
   interest=$(../entrypoint.sh $token ${1} no_token dryrun | grep -A 2 helloworld_github )
   if ! echo $interest | grep -q "${2}" ; then
-    echo Failed. Found:
+    echo Failed!
+    echo Found:
     echo $interest
     exit 1
   else
@@ -22,7 +23,7 @@ test_tf_update_action() {
 #  fi
 }
 
-test_tf_update_action modules/beta "1.12.0-beta1"
+test_tf_update_action modules/beta "1.12.1-beta1"
 
 test_tf_update_action modules/follow "~> 1.0"
 
