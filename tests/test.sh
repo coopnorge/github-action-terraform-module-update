afc7d2adf68bff096ea79cc1232c59aef493cd7a
Atze de Vries <AtzeDeVries@users.noreply.github.com>
2022-06-29 16:06:33 +0200

    test

--- tests/test.sh
+++ tests/test.sh
@@ -4,17 +4,24 @@
 token=${1}
 
 test_tf_update_action() {
-  DEBUG=enabeld ../entrypoint.sh $token ${1} no_token dryrun | grep "${2}"
-  status=$?
-  if [[ ! ${status} -eq 0 ]] ; then
-    echo Failed. Testing ${1}
-    echo Expected to find ${2}
+  echo Testing ${1}, expecting ${2}
+  interest=$(../entrypoint.sh $token ${1} no_token dryrun | grep -A 2 helloworld_github )
+  if ! echo $interest | grep -q ${2} ; then
+    echo Failed. Found:
+    echo $interest
     exit 1
   fi
+
+
+#  if [[ ! ${status} -eq 0 ]] ; then
+#    echo Failed. Testing ${1}
+#    echo Expected to find ${2}
+#    exit 1
+#  fi
 }
 
-test_tf_update_action modules/beta "-  version = \"1.12.0-beta1\""
+test_tf_update_action modules/beta "1.12.0-beta1"
 
-test_tf_update_action modules/follow "-  version = \"~> 1.0\""
+test_tf_update_action modules/follow "~> 1.0"
 
-test_tf_update_action modules/pinned "-  version = \"1.12.0\""
+test_tf_update_action modules/pinned "1.12.0"
