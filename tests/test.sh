17d7fec0d914de245d48e2e273cb5e727762d38d
Atze de Vries <AtzeDeVries@users.noreply.github.com>
2022-06-29 11:38:39 +0200

    fix command

--- tests/test.sh
+++ tests/test.sh
@@ -5,4 +5,4 @@ folder=${2:-modules/beta}
 
 
 echo Testing beta versions
-../entrypoint.sh $token modules/beta no_token dryrun | grep "-  version = \"1.12.0-beta1\""
+../entrypoint.sh $token modules/beta no_token dryrun #| grep "-  version = \"1.12.0-beta1\""
