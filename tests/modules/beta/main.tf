fe2496cb0e5cc4eb43aacf6a78dbddf3c1380f3d
Atze de Vries <AtzeDeVries@users.noreply.github.com>
2022-06-29 09:45:23 +0200

    updating $1 to $2

--- tests/modules/beta/main.tf
+++ tests/modules/beta/main.tf
@@ -1,5 +1,5 @@
 module "helloworld_github_repo" {
   source  = "terraform.coop.no/coopnorge/repos/github"
-  version = "1.12.0-beta1"
-  name                    = "helloworld"
+  version = "1.12.2"
+  name    = "helloworld"
 }
