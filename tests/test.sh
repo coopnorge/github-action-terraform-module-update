36a5241d029e6961c41d2f2eaf7b417cc842419e
Atze de Vries <AtzeDeVries@users.noreply.github.com>
2022-06-29 14:55:23 +0200

    fix test

--- tests/test.sh
+++ tests/test.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
 token=${1}
-folder=${2:-modules/beta}
+
 
 
 echo Testing beta versions
 ../entrypoint.sh $token modules/beta no_token dryrun | grep "\\-  version = \"1.12.0-beta1\""
+
+echo Testing follow versions
+../entrypoint.sh $token modules/follow no_token dryrun | grep "\\-  version = \"~> 1.0\""
+
+echo Testing pinned versions
+../entrypoint.sh $token modules/pinned no_token dryrun | grep "\\-  version = \"1.12.0\""
