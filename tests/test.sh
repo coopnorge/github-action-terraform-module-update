c79252e613b1c7566718354b09b3489f9e2a6894
Atze de Vries <AtzeDeVries@users.noreply.github.com>
2022-06-29 15:31:33 +0200

    fix test

--- tests/test.sh
+++ tests/test.sh
@@ -6,15 +6,19 @@ token=${1}
 test_tf_update_action() {
   ../entrypoint.sh $token ${1} no_token dryrun | grep "${2}"
   status=$?
-  echo $status
   if [[ ! ${status} -eq 0 ]] ; then
-    echo Failed. Expected to find ${2}
+    echo Failed. Testing ${1}
+    echo Expected to find ${2}
     exit 1
   fi
 }
 
 test_tf_update_action modules/beta "\\-  version = \"1.12.0-beta1\""
-exit
+
+test_tf_update_action modules/follow "\\-  version = \"~> 1.0\""
+
+test_tf_update_action modules/pinned "\\-  version = \"1.12.0\""
+
 
 
 echo Testing beta versions
